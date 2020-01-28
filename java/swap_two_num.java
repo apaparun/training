974f760f7d1145717fd5e75456da5c87752b39a0
ArunPrasanth A P <36165446+apaparun@users.noreply.github.com>
2020-01-28 11:33:05 +0530

    Create swap_two_num.java

---
+++ java/swap_two_num.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a,b;
+    a=sc.nextInt();
+    b=sc.nextInt();
+
+    a= a+b;
+    b=a-b;
+    a=a-b;
+    System.out.println(a+" "+b);
+  }
+}
